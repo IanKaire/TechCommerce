056a1bcd3123f801df95dc7b48fcf529bb86e175
Ian Kaire <iankaire@gmail.com>
2023-07-19 17:20:43 +0300

    added ProductCard component and ProductPage page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
   return (
     <ChakraProvider>
       <Router>
         <Navbar/>
+         <main>
+           <Routes>
+            <Route path='/products' element={<ProductsPage/>}></Route>
+            </Routes>
+         </main>
       </Router>
     </ChakraProvider> 
   );
---
+++ client/src/pages/ProductsPage.jsx
@@ -0,0 +1,19 @@
+import { Center, Wrap, WrapItem } from "@chakra-ui/react";
+import ProductCard from "../components/ProductCard";
+import { products } from "../products";
+
+const ProductsPage = () => {
+  return (
+    <Wrap spacing="30px" justify="center" minHeight="100vh">
+      {products.map((product) => (
+        <WrapItem key={product._id}>
+          <Center w="250px" h="550px">
+            <ProductCard product={product} />
+          </Center>
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
+};
+
+export default ProductsPage;
